e5f9c38e40ac772515a930b35cdff492106f708c
Phil Zona <pzona@launchdarkly.com>
2022-09-27 17:45:41 -0400

    Remove unused dependency

--- components/DynamicCode.js
+++ components/DynamicCode.js
@@ -2,8 +2,6 @@ import { useState, useEffect } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { getSdkKeyFromLocalStorage } from '../lib/flagHelpers';
 import { docco } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
-import { OPTIONS } from '../lib/constants';
-
 
 export default function DynamicCode() {
     let [sdkKey, setSdkKey] = useState('');
