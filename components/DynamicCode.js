2b8f3ad2fce98356fe1faef64919cad6695875e1
Phil Zona <pzona@launchdarkly.com>
2022-09-27 17:39:35 -0400

    Use localStorage polling to fill SDK code sample

--- components/DynamicCode.js
+++ components/DynamicCode.js
@@ -2,25 +2,29 @@ import { useState, useEffect } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { getSdkKeyFromLocalStorage } from '../lib/flagHelpers';
 import { docco } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
-import styles from './css/FlagKeyInput.module.css';
+import { OPTIONS } from '../lib/constants';
 
 
 export default function DynamicCode() {
     let [sdkKey, setSdkKey] = useState('');
 
+    useEffect(() => {
+      const interval = setInterval(() => {
+        updateCodeSample();
+      }, OPTIONS.refreshInterval);
+
+      return () => clearInterval(interval);
+    }, [])
+
     const updateCodeSample = () => {
-        setSdkKey(getSdkKeyFromLocalStorage());
+      setSdkKey(getSdkKeyFromLocalStorage());
     }
 
-
 return (
-  <>
   <SyntaxHighlighter showLineNumbers="true" startingLineNumber={14} language="javascript" style={docco}>
   {
   `  // STEP 2: Define your SDK key
     const SDK_KEY = '${ sdkKey }'` 
   }
   </SyntaxHighlighter>
-  <button onClick={updateCodeSample} className={styles.button}>Generate Code Sample</button>
-  </>
 )}
