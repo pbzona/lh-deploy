df88363efac9be7fd4699743f46c82a10cd5966c
Phil Zona <pzona@launchdarkly.com>
2022-10-04 16:12:11 -0400

    Dockerized 😎

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
-  unstable_staticImage: true,
-})
+  unstable_staticImage: true
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  output: 'standalone'
+});
