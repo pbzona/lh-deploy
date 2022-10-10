edcc873e1d254e416582b9f93059cfe752dee532
Phil Zona <pzona@launchdarkly.com>
2022-10-10 15:15:23 -0400

    update name

--- components/LDLogo.js
+++ components/LDLogo.js
@@ -5,7 +5,7 @@ export default function LDLogo({ height, width }) {
   return (
     <div className={style.wrapper}>
       <Image src='/svg/LaunchDarkly.svg' height={height} width={width} />
-      <span className={style.subText}>Lighthouse</span>
+      <span className={style.subText}>North Star</span>
     </div>
   )
 }
